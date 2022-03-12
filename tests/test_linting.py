61d1963305efe5e1f5e40f743dda21de8e89c324
Max Hirschhorn <max.hirschhorn@mongodb.com>
2022-03-12 19:24:54 +0000

    Lint stub files in stubs/ directory with PyLint.

--- tests/test_linting.py
+++ tests/test_linting.py
@@ -22,7 +22,7 @@ import pytest
 @pytest.mark.filterwarnings(r"ignore:In astroid 3.0.0 NodeNG.statement\(\).*:DeprecationWarning")
 def test_linting():
     """Check code and tests for Python linting errors."""
-    runner = pylint.lint.Run(["../gdbmongo/", "../tests/", "--rcfile=../pyproject.toml"],
-                             exit=False)
+    runner = pylint.lint.Run(
+        ["--rcfile=../pyproject.toml", "../gdbmongo/", "../stubs/", "../tests/"], exit=False)
     lint_ok = runner.linter.msg_status == 0
     assert lint_ok, "Changes are needed to address linting issues"
