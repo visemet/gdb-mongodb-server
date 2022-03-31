3a4664060d733bae1691b5ad8d780f8ba0b92060
Max Hirschhorn <max.hirschhorn@mongodb.com>
2022-03-31 03:21:00 +0000

    Add memory layout compatible ctypes structure for mongo::OID.

--- gdbmongo/objectid_printer.py
+++ gdbmongo/objectid_printer.py
@@ -15,9 +15,47 @@
 ###
 """Pretty-printer for the mongo::OID type."""
 
+import ctypes
+import dataclasses
+import typing
+
 import gdb
 
 from gdbmongo.printer_protocol import SupportsToString
+
+
+@dataclasses.dataclass
+class MongoOID(ctypes.Structure):
+    """Structure with a memory layout compatible with that of mongo::OID.
+
+    This class is useful for constructing gdb.Value objects of type mongo::OID out of selected
+    portions of a buffer read with gdb.Inferior.read_memory(). These synthetic gdb.Values can then
+    be formatted by ObjectIdPrinter like normal.
+
+    .. code-block:: python
+
+        objdata = gdb.selected_inferior().read_memory(self.val["_objdata"], objsize)
+        object_id = MongoOID.unpack_from(objdata)
+        yield (f"{i}", object_id.to_value())
+    """
+
+    if typing.TYPE_CHECKING:
+        data: ctypes.Array[ctypes.c_char]
+    else:
+        data: ctypes.c_char * 12
+
+    @classmethod
+    def unpack_from(cls, buffer: memoryview, /) -> "MongoOID":
+        """Read a 12-byte ObjectId starting from the beginning of the given buffer."""
+        return cls.from_buffer(buffer)
+
+    def to_value(self) -> gdb.Value:
+        """Convert the structure to a gdb.Value of type mongo::OID."""
+        typ = gdb.lookup_type("mongo::OID")
+        return gdb.Value(memoryview(self), typ)
+
+
+setattr(MongoOID, "_fields_", [(field.name, field.type) for field in dataclasses.fields(MongoOID)])
 
 
 # pylint: disable-next=too-few-public-methods
