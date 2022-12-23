6b52f9889d7240fd9d068bb4cb40008c2804e832
Max Hirschhorn <max.hirschhorn@mongodb.com>
2022-12-23 01:00:34 +0000

    Inline definition for strtobool() to accommodate PEP 632.
    Confirms that the Python unit tests are now passing under py310.
    Notably the MongoDB v4 toolchain uses Python 3.10.4.

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -15,7 +15,6 @@
 ###
 """Test file for checking Python formatting."""
 
-import distutils.util
 import itertools
 import os
 import pathlib
@@ -51,9 +50,29 @@ def run_yapf(fix: bool) -> bool:
     return ret == 0 or fix
 
 
+# Adapted from the definition for distutils.util.strtobool() in Python 3.11.1 to accommodate the
+# deprecation of the distutils module (PEP 632).
+def strtobool(val: str) -> bool:
+    """Convert a string representation of truth to True or False.
+
+    True values are "y", "yes", "t", "true", "on", and "1".
+    False values are "n", "no", "f", "false", "off", and "0".
+
+    Raises a ValueError if `val` is any other value.
+    """
+    val = val.lower()
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return True
+
+    if val in ("n", "no", "f", "false", "off", "0"):
+        return False
+
+    raise ValueError(f"Invalid truth value: {val!r}")
+
+
 @pytest.mark.filterwarnings(r"ignore:lib2to3 package is deprecated.*:PendingDeprecationWarning")
 def test_formatting() -> None:
     """Check code and tests for Python formatting errors."""
-    should_fix = distutils.util.strtobool(os.environ.get("TOX_YAPF_FIX", "0"))
+    should_fix = strtobool(os.environ.get("TOX_YAPF_FIX", "0"))
     format_ok = run_yapf(should_fix)
     assert format_ok, "Changes are needed to address formatting issues; try running `tox -e format`"
