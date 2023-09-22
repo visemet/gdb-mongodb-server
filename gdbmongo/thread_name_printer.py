c01a6feaa70b4a1d3da344a77aa125649fa9ae7e
Max Hirschhorn <max.hirschhorn@mongodb.com>
2023-09-22 03:20:33 +0000

    Support getting thread name from ThreadNameInfo decoration.

--- gdbmongo/thread_name_printer.py
+++ gdbmongo/thread_name_printer.py
@@ -20,6 +20,7 @@ import typing
 import gdb
 
 from gdbmongo import stdlib_printers
+from gdbmongo.decorable_printer import DecorationIterator
 from gdbmongo.printer_protocol import SupportsDisplayHint
 from gdbmongo.string_data_printer import StdStringPrinter, ValueAsPythonStringMixin
 
@@ -35,35 +36,64 @@ def get_thread_name() -> str:
     thread's name is recorded in a core dump.
     """
     try:
+        # The ThreadNameInfo type replaced the ThreadNameSconce type for representing the storage
+        # for the thread name as part of SERVER-63852 in MongoDB 6.1 and was then subsequently
+        # backported to 5.0.12 and 6.0.2. In some versions the ThreadNameInfo instance is managed
+        # directly as a thread-local variable, and in other versions it is managed as a decoration
+        # within the ThreadContext object.
+        thread_info_type = gdb.lookup_type("mongo::(anonymous namespace)::ThreadNameInfo")
+    except gdb.error as err:
+        if not err.args[0].startswith("No type named "):
+            raise
+
+        # Supporting older versions will also need to consider the changes from SERVER-52821.
+        return ""
+
+    try:
         # The `tls` thread-local variable was introduced to the ThreadNameInfo::forThisThread()
-        # static function as part of SERVER-66385 in MongoDB 6.1. Previously in MongoDB 6.0, the
-        # thread name was stored as a decoration within the ThreadContext object.
-        thread_info = gdb.parse_and_eval(
-            "&'mongo::(anonymous namespace)::ThreadNameInfo::forThisThread()::tls'").dereference()
+        # static function as part of SERVER-66385 in MongoDB 6.1. The ThreadNameInfo instance was
+        # previously managed as a decoration within the ThreadContext object.
+        thread_info_pp = gdb.parse_and_eval(
+            "&'mongo::(anonymous namespace)::ThreadNameInfo::forThisThread()::tls'")
     except gdb.error as err:
         if not err.args[0].startswith("No symbol "):
             raise
 
-        # Supporting older versions will also need to consider the changes from SERVER-52821 and
-        # SERVER-63852.
-        return ""
+        if (thread_context := _get_thread_context()) == 0:
+            return ""
 
-    # The 'mongo::(anonymous namespace)::ThreadNameInfo::forThisThread():Tls' struct has a trivial
-    # definition and its typeinfo can be elided.
-    #
-    #   struct Tls {
-    #       ThreadNameInfo* info = new ThreadNameInfo;
-    #   };
-    #
-    #   (gdb) print 'mongo::(anonymous namespace)::ThreadNameInfo::forThisThread()::tls'
-    #   '[...]ThreadNameInfo::forThisThread()::tls' has unknown type; cast it to its declared type
-    #
-    # We therefore cast what would be the Tls* to be a ThreadNameInfo* because the Tls::info member
-    # variable is guaranteed to be stored at byte offset 0 of the struct.
-    thread_info = thread_info.cast(
-        gdb.lookup_type("mongo::(anonymous namespace)::ThreadNameInfo").pointer())
+        for decoration in DecorationIterator(thread_context.dereference()):
+            if decoration.type == thread_info_type:
+                thread_info = decoration.address
+                break
+        else:
+            raise ValueError(
+                "Failed to locate ThreadNameInfo decoration in ThreadContext") from None
+    else:
+        # The 'mongo::(anonymous namespace)::ThreadNameInfo::forThisThread():Tls' struct has a
+        # trivial definition and its typeinfo can be elided.
+        #
+        #   struct Tls {
+        #       ThreadNameInfo* info = new ThreadNameInfo;
+        #   };
+        #
+        #   (gdb) print 'mongo::(anonymous namespace)::ThreadNameInfo::forThisThread()::tls'
+        #   'mongo::(anonymous namespace)::ThreadNameInfo::forThisThread()::tls' has unknown type;
+        #   cast it to its declared type
+        #
+        # We therefore cast what would be the Tls* to be a ThreadNameInfo** because the Tls::info
+        # member variable is guaranteed to be stored at byte offset 0 of the struct.
+        thread_info = thread_info_pp.cast(thread_info_type.pointer().pointer()).dereference()
 
-    return _ThreadNameInfoPrinter(thread_info).string() if thread_info != 0 else ""
+    return _ThreadNameInfoPrinter(thread_info.dereference()).string() if thread_info != 0 else ""
+
+
+def _get_thread_context() -> gdb.Value:
+    """Return the ThreadContext object associated with the current thread."""
+    thread_context_handle_p = gdb.parse_and_eval("&mongo::ThreadContext::_handle")
+    thread_context_handle_type = gdb.lookup_type("mongo::ThreadContext::Handle")
+    thread_context_handle_p = thread_context_handle_p.cast(thread_context_handle_type.pointer())
+    return thread_context_handle_p.dereference()["instance"]["px"]
 
 
 # mongo::(anonymous namespace)::ThreadNameInfo isn't a type which is likely to be printed so we
