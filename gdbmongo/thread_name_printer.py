bf275516a5978e0436f6376aa0b47ab8860526c5
Max Hirschhorn <max.hirschhorn@mongodb.com>
2023-09-22 04:33:21 +0000

    Support getting thread name from ThreadNameSconce decoration.
    Also adds support for getting thread name from thread-local
    `mongo::for_debuggers::threadName` variable.

--- gdbmongo/thread_name_printer.py
+++ gdbmongo/thread_name_printer.py
@@ -22,7 +22,8 @@ import gdb
 from gdbmongo import stdlib_printers
 from gdbmongo.decorable_printer import DecorationIterator
 from gdbmongo.printer_protocol import SupportsDisplayHint
-from gdbmongo.string_data_printer import StdStringPrinter, ValueAsPythonStringMixin
+from gdbmongo.string_data_printer import (StdStringPrinter, StringDataPrinter,
+                                          ValueAsPythonStringMixin)
 
 
 def get_thread_name() -> str:
@@ -46,8 +47,20 @@ def get_thread_name() -> str:
         if not err.args[0].startswith("No type named "):
             raise
 
-        # Supporting older versions will also need to consider the changes from SERVER-52821.
-        return ""
+        try:
+            # The ThreadNameSconce type was introduced for representing the storage for the thread
+            # name as part of SERVER-52821 in MongoDB 5.0. It is always managed as a decoration
+            # within the ThreadContext object. Previously in MongoDB 4.4, the thread name was made
+            # available through a thread-local StringData variable.
+            thread_sconce_type = gdb.lookup_type("mongo::(anonymous namespace)::ThreadNameSconce")
+        except gdb.error as err2:
+            if not err2.args[0].startswith("No type named "):
+                raise
+
+            thread_name = gdb.parse_and_eval("mongo::for_debuggers::threadName")
+            return StringDataPrinter(thread_name).string()
+        else:
+            return _get_thread_name_from_sconce(thread_sconce_type)
 
     try:
         # The `tls` thread-local variable was introduced to the ThreadNameInfo::forThisThread()
@@ -88,6 +101,24 @@ def get_thread_name() -> str:
     return _ThreadNameInfoPrinter(thread_info.dereference()).string() if thread_info != 0 else ""
 
 
+def _get_thread_name_from_sconce(thread_sconce_type: gdb.Type, /) -> str:
+    """Return the thread name stored within the ThreadNameSconce instance."""
+    if (thread_context := _get_thread_context()) == 0:
+        return ""
+
+    for decoration in DecorationIterator(thread_context.dereference()):
+        if decoration.type == thread_sconce_type:
+            thread_sconce = decoration.address
+            break
+    else:
+        raise ValueError("Failed to locate ThreadNameSconce decoration in ThreadContext")
+
+    if (thread_name := thread_sconce["activePtr"]["px"]) == 0:
+        thread_name = thread_sconce["cachedPtr"]["px"]
+
+    return _ThreadNamePrinter(thread_name.dereference()).string() if thread_name != 0 else ""
+
+
 def _get_thread_context() -> gdb.Value:
     """Return the ThreadContext object associated with the current thread."""
     thread_context_handle_p = gdb.parse_and_eval("&mongo::ThreadContext::_handle")
@@ -114,3 +145,20 @@ class _ThreadNameInfoPrinter(SupportsDisplayHint, ValueAsPythonStringMixin):
             "std::shared_ptr", self.val["_h"]["_ptr"]).pointer.dereference()
 
         return StdStringPrinter(thread_name).string()
+
+
+# mongo::ThreadName isn't a type which is likely to be printed so we don't bother registering it
+# with GDB.
+class _ThreadNamePrinter(SupportsDisplayHint, ValueAsPythonStringMixin):
+    # pylint: disable=missing-function-docstring
+    """Pretty-printer for mongo::ThreadName."""
+
+    def __init__(self, val: gdb.Value, /) -> None:
+        self.val = val
+
+    @staticmethod
+    def display_hint() -> typing.Literal["string"]:
+        return "string"
+
+    def to_string(self) -> str:
+        return StdStringPrinter(self.val["_storage"]).string()
